3bd0cd466b642578d91e30c962596eb85acda617
miao <920378797@qq.com>
2020-07-16 10:42:09 +0800

    修改

--- first_git/src/com/itheima/git/FirstClass.java
+++ first_git/src/com/itheima/git/FirstClass.java
@@ -3,8 +3,8 @@ package com.itheima.git;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hello git");
-    }
-
+    }//
+    //hello test
     public  void test(){
        int a= 1+1;
     }
